51cb9deb84c3de3fb2d799bd17fa8be1e322b20e
TheLegendz-DaBest <76889737+TheLegendz-DaBest@users.noreply.github.com>
2021-01-16 18:45:50 -0600

    Update and rename README.md to Settings.lua

---
+++ Settings.lua
@@ -0,0 +1,9 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {
+  Title = "AIMBOT",
+  Text = "Press L.Ctrl"
+})
+ 
+game:GetService("StarterGui"):SetCore("SendNotification", {
+  Title = "ESP NAME",
+  Text = "Press L.Alt"
+})
